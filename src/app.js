1c8188d1bd93654f1999366b12a1d4b1c4178ab3
manasnayak-dev <nayakmanasbivu@gmail.com>
2026-03-08 10:34:07 +0530

    change the frontend URL

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "dev-tinder-frontend-9t3x0mz0f-manasnayak-devs-projects.vercel.app",
+    origin: "dev-tinder-frontend-two-phi.vercel.app",
     credentials: true,
   }),
 );
